12ac908b88d32dbbb7f44992eecf814f061ed7d1
Nikolai <nickpon@mail.ru>
2019-03-06 23:13:56 +0300

    Initail commit with completed homework.

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,80 +1,50 @@
 package ru.sbt.mipt.oop;
 
-import com.google.gson.Gson;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import static ru.sbt.mipt.oop.SensorEventType.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Application {
 
+    private static SmartHomeApplication smartHomeApplication = new ToFileSmartHome();
+
     public static void main(String... args) throws IOException {
-        // считываем состояние дома из файла
-        Gson gson = new Gson();
-        String json = new String(Files.readAllBytes(Paths.get("smart-home-1.js")));
-        SmartHome smartHome = gson.fromJson(json, SmartHome.class);
-        // начинаем цикл обработки событий
-        SensorEvent event = getNextSensorEvent();
-        while (event != null) {
-            System.out.println("Got event: " + event);
-            if (event.getType() == LIGHT_ON || event.getType() == LIGHT_OFF) {
-                // событие от источника света
-                for (Room room : smartHome.getRooms()) {
-                    for (Light light : room.getLights()) {
-                        if (light.getId().equals(event.getObjectId())) {
-                            if (event.getType() == LIGHT_ON) {
-                                light.setOn(true);
-                                System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
-                            } else {
-                                light.setOn(false);
-                                System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned off.");
-                            }
-                        }
-                    }
-                }
+        SmartHome smartHome = smartHomeApplication.loadSmartHome();
+
+        goThroughLoops(smartHome);
+    }
+
+    private static void goThroughLoops(SmartHome smartHome) {
+        SensorEvent nextEvent = RandomSensorEventProvider.getNextEvent();
+        Collection<EventAggregator> eventAggregators = configureEventProcessors();
+        makeLoops(nextEvent, eventAggregators, smartHome);
+    }
+
+    private static void makeLoops (SensorEvent nextEvent, Collection<EventAggregator> eventAggregators,
+                                   SmartHome smartHome) {
+        while (nextEvent != null) {
+            System.out.println("Event: " + nextEvent);
+            for (EventAggregator eventAggregator : eventAggregators) {
+                eventAggregator.processEvent(smartHome, nextEvent);
             }
-            if (event.getType() == DOOR_OPEN || event.getType() == DOOR_CLOSED) {
-                // событие от двери
-                for (Room room : smartHome.getRooms()) {
-                    for (Door door : room.getDoors()) {
-                        if (door.getId().equals(event.getObjectId())) {
-                            if (event.getType() == DOOR_OPEN) {
-                                door.setOpen(true);
-                                System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
-                            } else {
-                                door.setOpen(false);
-                                System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
-                                // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
-                                // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
-                                if (room.getName().equals("hall")) {
-                                    for (Room homeRoom : smartHome.getRooms()) {
-                                        for (Light light : homeRoom.getLights()) {
-                                            light.setOn(false);
-                                            SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
-                                            sendCommand(command);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            event = getNextSensorEvent();
+            nextEvent = RandomSensorEventProvider.getNextEvent();
         }
     }
 
-    private static void sendCommand(SensorCommand command) {
-        System.out.println("Pretent we're sending command " + command);
+    public static void setSmartHomeApplication(SmartHomeApplication smartHomeApplication) {
+        Application.smartHomeApplication = smartHomeApplication;
     }
 
-    private static SensorEvent getNextSensorEvent() {
-        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
-        if (Math.random() < 0.05) return null; // null means end of event stream
-        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
-        String objectId = "" + ((int) (10 * Math.random()));
-        return new SensorEvent(sensorEventType, objectId);
+    private static Collection<EventAggregator> configureEventProcessors() {
+        Collection<EventAggregator> eventAggregators = new ArrayList<>();
+        modifyAggregators(eventAggregators);
+        return eventAggregators;
     }
+
+    private static void modifyAggregators(Collection<EventAggregator> eventAggregators) {
+        eventAggregators.add(new LightsEventAggregator());
+        eventAggregators.add(new DoorEventAggregator());
+        eventAggregators.add(new MainDoorEventAggregator());
+    }
+
 }
